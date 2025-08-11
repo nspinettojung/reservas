0f33dc4dae6d2f8fe407aa758ace1fd19e05f8cf
Nicolás Spinetto Jung <nicospinetto@gmail.com>
2025-08-11 19:26:56 -0300

    sidenav routerlinkactive

--- src/app/page/reservas/reservas.ts
+++ src/app/page/reservas/reservas.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Calendario } from "../../shared/calendario/calendario";
+import { Sidenav } from "../../shared/sidenav/sidenav";
 
 @Component({
   selector: 'app-reservas',
-  imports: [],
+  imports: [Calendario, Sidenav],
   templateUrl: './reservas.html',
   styleUrl: './reservas.scss'
 })
--- src/app/shared/sidenav/sidenav.ts
+++ src/app/shared/sidenav/sidenav.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'ara-sidenav',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './sidenav.html',
   styleUrl: './sidenav.scss',
 })
