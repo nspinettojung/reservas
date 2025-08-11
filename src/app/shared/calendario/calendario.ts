37e0949a70c2c40d43e78d7334fab783a482e16c
Nicolás Spinetto Jung <nicospinetto@gmail.com>
2025-08-11 10:14:38 -0300

    nuevo proyecto reserva

---
+++ src/app/shared/calendario/calendario.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendario',
+  imports: [],
+  templateUrl: './calendario.html',
+  styleUrl: './calendario.scss'
+})
+export class Calendario {
+
+}
---
+++ src/app/shared/lista-cliente/lista-cliente.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-cliente',
+  imports: [],
+  templateUrl: './lista-cliente.html',
+  styleUrl: './lista-cliente.scss'
+})
+export class ListaCliente {
+
+}
